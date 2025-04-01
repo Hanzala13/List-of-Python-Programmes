93613b31fd29e97099a756a26f89f055172cd0bf
Hanzala13 <azmihanzala123456789@gmail.com>
2025-04-01 21:10:43 +0530

    ASCII

---
+++ ASCII.py
@@ -0,0 +1,6 @@
+# Program to find the ASCII value of a character
+
+char = input("Enter a character: ")  # Taking input from user
+ascii_value = ord(char)  # Getting ASCII value using ord() function
+
+print(f"The ASCII value of '{char}' is {ascii_value}")
