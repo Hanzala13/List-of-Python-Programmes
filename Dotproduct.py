e1d76d69bfd63e5eecc0e527f4e2f6b04cd8cc90
Hanzala13 <azmihanzala123456789@gmail.com>
2025-05-13 19:35:12 +0530

    Dot product

---
+++ Dotproduct.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Example NumPy array
+arr = np.array([1, 2, 3])
+
+# Dot product with itself
+dot_product = np.dot(arr, arr)
+
+# Or using the @ operator
+# dot_product = arr @ arr
+
+print(dot_product)
